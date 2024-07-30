d417849e97851d539e2986c32237ab2a5dd91513
joaomarcosmr <joaomarcosmr4@gmail.com>
2024-07-29 21:16:00 -0300

    alterations for upload

--- models/Forms.js
+++ models/Forms.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
+const User = require('./User');
 
 const formsSchema = new mongoose.Schema({
 	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
+	phone: {
 		type: String,
 		required: true
 	},
 	customFields: {
 		type: Map,
-		of: mongoose.Schema.Types.Mixed // This allows any type of value (String, Number, etc.)
+		of: mongoose.Schema.Types.Mixed
+	},
+	status: {
+		type: String,
+		enum: ['new', 'sent', 'backlog'],
+		default: 'new'
 	}
 }, { strict: false });
 
-module.exports = Forms = mongoose.model('forms', formsSchema)
+module.exports = mongoose.model('Form', formsSchema);
